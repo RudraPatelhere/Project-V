77d53cf4fe6229c3e52b9dc16b7b73ac757546df
RudraPatelhere <Rpatel1198@conestogac.on.ca>
2024-09-23 11:16:46 -0400

    Step-4 Updated (Just Debug)

--- Assignment2/Source.cpp
+++ Assignment2/Source.cpp
@@ -3,61 +3,66 @@
 #include <vector>
 #include <sstream>
 
-// We will make function for the students which will store the first and last names 
+// Define a struct to hold the student's first name and last name
 struct STUDENT_DATA
 {
-    std::string FIRST_NAME;
-    std::string LAST_NAME;
+    std::string FIRST_NAME;  // Variable to store the student's first name
+    std::string LAST_NAME;   // Variable to store the student's last name
 };
 
-// As instruction given we will declare vector to store all the data's
+std::vector<STUDENT_DATA> students;  // Declare a vector to store all the student data
 
-std::vector<STUDENT_DATA> students;
-
-// We will make function to load student data from given file
+// Function to load student data from the given file
 void LOAD_STUDENTS_DATA(const std::string& fileName)
 {
-    std::ifstream SOURCE_FILE(fileName);  // This will ppen the file
-    std::string LINES;
+    std::ifstream SOURCE_FILE(fileName);  // This will open the file with the given file name
+    std::string LINE;  // Variable to hold each line read from the file
 
-    // As we have to check if the file opened successfully
-    if ( SOURCE_FILE.is_open() )
+    // Check if the file opened successfully
+    if (SOURCE_FILE.is_open())
     {
-        while ( getline(SOURCE_FILE, LINES) )
+        // Loop through each line in the file
+        while (getline(SOURCE_FILE, LINE))
         {
-            std::stringstream SPLIT_LINES(LINES);  // As per instruction we have to split the line into first/last name
-            std::string FIRST_NAMES, LAST_NAMES;
+            std::stringstream SPLIT_LINE(LINE);  // Create a stringstream to split the line into parts
+            std::string FIRST_NAME;  // Variable to hold the first name
+            std::string LAST_NAME;   // Variable to hold the last name
 
-            // USing comma to seperate first and last name
-            if (getline(SPLIT_LINES, FIRST_NAMES, ',') && getline(SPLIT_LINES, LAST_NAMES))
+            // Parse the first name and last name
+            if (getline(SPLIT_LINE, FIRST_NAME, ',') &&
+                getline(SPLIT_LINE, LAST_NAME))
             {
-                STUDENT_DATA STUDENTS;  // We will create a object STUDENT_DATA 
-                STUDENTS.FIRST_NAME = FIRST_NAMES; // Will make object of firstname
-                STUDENTS.LAST_NAME = LAST_NAMES; // Will make object of lastname
+                STUDENT_DATA student;  // Create an instance of STUDENT_DATA
+                student.FIRST_NAME = FIRST_NAME;  // Set the first name of the student
+                student.LAST_NAME = LAST_NAME;    // Set the last name of the student
 
-                // WE will Push the student object in the vector
-                students.push_back(STUDENTS);
+                // Push the student object into the vector
+                students.push_back(student);
             }
         }
-        SOURCE_FILE.close();  // Will close the file after featchin all the data
+        SOURCE_FILE.close();  // Close the file after fetching all the data
     }
-    else // error message if somthig went wrong
+    else  // Error message if something went wrong
     {
-        std::cout << "wOOPS something went wrong cannot open the file:  " << fileName << std::endl;
+        std::cout << "Oops, something went wrong. Cannot open the file: " << fileName << std::endl;
     }
 }
 
-int main() 
+int main()
 {
-    // We will call the function to load the datas from the given file
+#ifdef _DEBUG
+    std::cout << "Running in Debug mode" << std::endl;  // Print a message for debug mode
+    // Load data
     LOAD_STUDENTS_DATA("StudentData.txt");
 
-    // This will print the student data to verify it was loaded perfectly.
-    for (const auto& STUDENTS: students) 
+    // Print the student data to verify it was loaded correctly
+    for (const auto& student : students)
     {
-        // This statment will print each student's first and last name
-        std::cout << "First Name: " << STUDENTS.FIRST_NAME << ", Last Name: " << STUDENTS.LAST_NAME << std::endl;
+        std::cout << "First Name: " << student.FIRST_NAME << ", Last Name: " << student.LAST_NAME << std::endl;
     }
+#else
+    std::cout << "This is standard mode" << std::endl;  // Message for non-debug mode
+#endif
 
-    return 1;
+    return 1;  // Return 1 to indicate successful execution
 }
